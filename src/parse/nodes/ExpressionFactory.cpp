98178e6f2996d030c856211a6564f17a6bbce8cb
= <=>
2026-09-09 15:37:28 +0200

    tixed typo

---
+++ src/parse/nodes/ExpressionFactory.cpp
@@ -0,0 +1,3 @@
+#include "ExpressionFactory.h"
+
+namespace marex::parse {}
---
+++ src/parse/nodes/ExpressionFactory.h
@@ -0,0 +1,11 @@
+#ifdef MAREX_PARSE_EXPRESSIONFACTORY_H
+#define MAREX_PARSE_EXPRESSIONFACTORY_H
+namespace marex::parse {
+class Expression;
+
+class ExpressionFactory final {
+   public:
+   private:
+};
+}  // namespace marex::parse
+#endif  // MAREX_PARSE_EXPRESSIONFACTORY_H
