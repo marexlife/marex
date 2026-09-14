7375313660f86b136a7976ee0cef8f96bac1c007
marcel <marcel.hajek7@gmail.com>
2026-09-14 20:13:19 +0200

    made run.py simpler

--- run.py
+++ run.py
@@ -1,26 +1,12 @@
 from subprocess import run
-from enum import Enum
 import os
 
-class BuildType(Enum):
-    RELEASE = 0
-    DEBUG = 1
-
-def build_type_str(BUILD_TYPE: BuildType) -> str:
-    match BUILD_TYPE:
-        case BuildType.RELEASE:
-            return "Release"
-        case BuildType.DEBUG:
-            return "Debug"
-
-BUILD_TYPE=BuildType.RELEASE
-
-BUILD_TYPE_STRING = build_type_str(BUILD_TYPE)
-
-run(["cmake", ".", "-B", "build", f"-DCMAKE_BUILD_TYPE={BUILD_TYPE_STRING}"], check=True)
+run(["cmake", ".", "-B", "build"], check=True)
 run(["cmake", "--build", "build"], check=True)
 
+ARGUMENTS=["./mrx/main.mrx"]
+
 if os.name == 'nt': 
-    run([f"./build/src/main/Debug/main.exe"])
+    run([f"./build/src/main/Debug/main.exe"] + ARGUMENTS)
 else:
-    run(["./build/src/main/main"])
+    run(["./build/src/main/main"] + ARGUMENTS)
