44bf957c4b854b83a9021e03a1bb951c5859091e
= <=>
2026-09-16 06:14:22 +0200

    renamed files

--- src/fetch/fetch.cpp
+++ src/fetch/fetch.cpp
@@ -1,11 +1,13 @@
-#include "Fetcher.h"
+#include "fetch.h"
 
 #include <fstream>
 #include <string>
 #include <string_view>
 
-namespace marex::fetch {
-[[nodiscard]] std::pmr::string Fetcher::run(
+namespace marex {
+static const std::size_t result_reserve_amount = 100;
+
+[[nodiscard]] std::pmr::string fetch::fetch(
     std::string_view filepath) {
     std::ifstream stream{filepath.data()};
     std::pmr::string result;
@@ -20,4 +22,4 @@ namespace marex::fetch {
 
     return result;
 }
-}  // namespace marex::fetch
+}  // namespace marex
