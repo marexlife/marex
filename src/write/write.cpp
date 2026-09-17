22f41aee7266fe0cc80a3c5a5e492a1409bedfcd
= <=>
2026-09-17 04:20:50 +0200

    made more redable

--- src/write/write.cpp
+++ src/write/write.cpp
@@ -7,11 +7,10 @@
 #include "logging.h"
 
 namespace marex {
+static const std::string_view output_file = "main.c";
+
 void write::write(
     parse::TranslationUnit&& translation_unit) {
-    static const std::string_view output_file =
-        "main.c";
-
     std::ofstream stream{output_file.data()};
 
     stream << translation_unit.as_c();
