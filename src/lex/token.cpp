9fccae38e1933fa6d5f4e676cdca566f61d02ca1
= <=>
2026-09-17 05:00:01 +0200

    renamed

---
+++ src/lex/token.cpp
@@ -0,0 +1,56 @@
+#include "token.h"
+
+#include <optional>
+#include <utility>
+
+#include "TokenKind.h"
+#include "binding_rank.h"
+#include "logging.h"
+#include "source_pos.h"
+
+namespace marex::lex {
+Token::Token(
+    [[maybe_unused]] core::Passkey<TokenFactory>&&
+        passkey,
+    std::string&& lexeme, TokenKind token_kind,
+    SourcePos source_pos)
+    : lexeme(std::move(lexeme)),
+      kind(token_kind),
+      source_pos(source_pos) {}
+
+[[nodiscard]] BindingRank Token::get_binding_rank()
+    const {
+    switch (kind) {
+        case lex::TokenKind::Var:
+            return BindingRank::Var;
+        case lex::TokenKind::Identifier:
+            return BindingRank::Invalid;
+        case TokenKind::None:
+            core::log_fatal_error("TokenKind is none");
+        default:
+            core::log_fatal_error(
+                "TokenKind is Invalid");
+    }
+}
+
+[[nodiscard]] std::string_view Token::get_lexeme()
+    const {
+    if (!lexeme) [[unlikely]] {
+        core::log_fatal_internal_error(
+            "trying to get lexeme when none is there");
+    }
+
+    return *lexeme;
+}
+
+[[nodiscard]] std::pmr::string
+Token::move_out_lexeme() {
+    if (!lexeme) [[unlikely]] {
+        core::log_fatal_internal_error(
+            "trying to move out a lexeme when none "
+            "exists");
+    }
+
+    return *lexeme;
+}
+}  // namespace marex::lex
---
+++ src/lex/token.h
@@ -0,0 +1,42 @@
+#ifndef MAREX_LEX_TOKEN_H
+#define MAREX_LEX_TOKEN_H
+#include <optional>
+#include <string>
+#include <string_view>
+
+#include "TokenKind.h"
+#include "passkey.h"
+#include "source_pos.h"
+
+namespace marex::lex {
+enum struct BindingRank : std::uint8_t;
+
+class [[nodiscard]] TokenFactory;
+class [[nodiscard]] Token final {
+   public:
+    Token([[maybe_unused]] core::Passkey<
+              TokenFactory>&& passkey,
+          std::string&& lexeme, TokenKind token_kind,
+          SourcePos source_pos);
+
+    [[nodiscard]] std::string_view get_lexeme() const;
+
+    [[nodiscard]] std::pmr::string move_out_lexeme();
+
+    [[nodiscard]] TokenKind get_kind() const {
+        return kind;
+    }
+
+    [[nodiscard]] BindingRank get_binding_rank() const;
+    [[nodiscard]] SourcePos get_pos() const {
+        return source_pos;
+    }
+
+   private:
+    std::optional<std::pmr::string> lexeme =
+        std::nullopt;
+    TokenKind kind{};
+    SourcePos source_pos;
+};
+}  // namespace marex::lex
+#endif  // MAREX_LEX_TOKEN_H
