6a7bfb6cdef1397336897c2713e6c4538242d734
= <=>
2026-09-17 22:17:10 +0200

    changed naming

--- src/parse/nodes/binary_op.cpp
+++ src/parse/nodes/binary_op.cpp
@@ -1,16 +1,16 @@
-#include "BinaryOp.h"
+#include "binary_op.h"
 
 #include <utility>
 
 namespace marex::parse {
 BinaryOp::BinaryOp(lex::Token&& token,
-                  BinaryOpKind binary_op_kind)
+                   BinaryOpKind binary_op_kind)
     : Expr(std::move(token)),
-      binary_op_kind(binary_op_kind) {}
+      binary_op_kind_(binary_op_kind) {}
 
 void BinaryOp::set(std::unique_ptr<Expr> lhs,
                    std::unique_ptr<Expr> rhs) {
-    this->lhs = std::move(lhs);
-    this->rhs = std::move(rhs);
+    this->lhs_ = std::move(lhs);
+    this->rhs_ = std::move(rhs);
 }
 }  // namespace marex::parse
--- src/parse/nodes/binary_op.h
+++ src/parse/nodes/binary_op.h
@@ -3,8 +3,8 @@
 #include <cstdint>
 #include <memory>
 
-#include "token.h"
 #include "nodes/Expr.h"
+#include "token.h"
 
 namespace marex::parse {
 enum struct [[nodiscard]] BinaryOpKind : std::uint8_t {
@@ -24,13 +24,13 @@ class BinaryOp final : public Expr {
              std::unique_ptr<Expr> rhs);
 
     [[nodiscard]] BinaryOpKind get_binary_op() const {
-        return binary_op_kind;
+        return binary_op_kind_;
     }
 
    private:
-    BinaryOpKind binary_op_kind{};
-    std::unique_ptr<Expr> lhs;
-    std::unique_ptr<Expr> rhs;
+    BinaryOpKind binary_op_kind_{};
+    std::unique_ptr<Expr> lhs_;
+    std::unique_ptr<Expr> rhs_;
 };
 }  // namespace marex::parse
 #endif  // MAREX_PARSE_BINARYOP_H
