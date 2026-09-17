456640aabb5069fe64119aa72bf5642333e46308
= <=>
2026-09-17 04:16:07 +0200

    chaning log

--- src/lex/LexerPrinter.cpp
+++ src/lex/LexerPrinter.cpp
@@ -12,9 +12,9 @@ void lex::print_tokens(
     core::log_info("\nLexer print start\n");
 
     for (lex::Token& token : token_stream) {
-        core::log_info(std::format(
+        core::log_info(
             "Token lexeme: '{}', Token kind: '{}'\n",
-            *token.get_kind(), token.get_lexeme()));
+            *token.get_kind(), token.get_lexeme());
     }
 
     core::log_info("Lexer print end\n\n");
