7eaf887cff808fca8869497709e9dd9f448eb463
= <=>
2026-09-23 01:06:06 +0200

    removed includes

--- src/parse/nodes/expr_build.cpp
+++ src/parse/nodes/expr_build.cpp
@@ -1,29 +1,13 @@
 #include "expr_build.h"
 
-#include <cstddef>
-#include <cstdint>
-#include <functional>
-#include <limits>
 #include <memory>
-#include <optional>
-#include <ranges>
 #include <stdexcept>
-#include <type_traits>
-#include <utility>
-#include <vector>
 
-#include "binding_rank.h"
-#include "defer.h"
-#include "expr_factory.h"
-#include "logging.h"
 #include "nodes/expr.h"
 #include "token.h"
-#include "token_kind.h"
 #include "token_stream.h"
 
 namespace marex {
-namespace parse {}  // namespace parse
-
 std::unique_ptr<parse::Expr> parse::build_expr(
     TokenStream& stream) {
     [[maybe_unused]] bool previous_was_more_powerful =
