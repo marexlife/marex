e6e4140f72294d36e4ff6192a0e1e68b5043976a
= <=>
2026-09-24 17:35:52 +0200

    tests less depenent on run.sh now

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
-COMPILER_PATH=./build/src/main/main
-MAREX_FILE=./marex/Main.marex
-
 cmake . -B build
 cmake --build build
-cd build/src/lex/tests && ctest . --output-on-failure
 
-${COMPILER_PATH} ${MAREX_FILE}
+./build/src/main/main ./marex/Main.marex
