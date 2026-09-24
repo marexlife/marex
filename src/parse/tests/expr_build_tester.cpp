60167d70518fd48ebde3206e912de2b489c05513
= <=>
2026-09-24 22:30:20 +0200

    improved build expr test

--- src/parse/tests/expr_build_tester.cpp
+++ src/parse/tests/expr_build_tester.cpp
@@ -1,14 +1,17 @@
 #include "expr_build_tester.h"
 
+#include <memory>
+
 #include "nodes/expr_build.h"
+#include "token_kind.h"
 #include "token_stream.h"
 
 namespace marex::parse::tests {
 int ExprBuildTester::test_expr_build() {
-    TokenStream token_stream{};
+    TokenStream token_stream;
 
     auto result = build_expr(token_stream);
 
-    return 0;
+    return result->get_kind() == lex::TokenKind::OpMul ? 0 : -1;
 }
 }  // namespace marex::parse::tests
